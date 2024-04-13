54c7f0f90bd454789c877fffadc956c3143a1fed
Bernardus Molkenboer <bart@backdigital.nl>
2024-04-13 10:31:56 +0700

    Setup Desktop Footer

--- src/layout/DesktopFooter.tsx
+++ src/layout/DesktopFooter.tsx
@@ -1,13 +1,129 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 function DesktopFooter() {
   return (
-    <section className="bg-customGray w-full px-5 ">
+    <section className="bg-customGray w-full px-5 font-poppins text-base text-darkGray">
       <div className="max-w-screen-xl mx-auto">
         <div className="flex">
-          <div className="flex-1">D</div>
-          <div className="flex-1">D</div>
-          <div className="flex-1 bg-blue">test</div>
+          <div className="flex-1 pt-10">
+            <h3 className="uppercase  text-h3 text-darkGray mb-10">Contact</h3>
+            <div className="flex flex-col">
+              <div className="w-full flex items-center mb-6">
+                <div className="mail" />
+                <Link
+                  href="mailto:info@proranje.nl"
+                  className="text-secondary text-base"
+                >
+                  Info@proranje.nl
+                </Link>
+              </div>
+              <div className="w-full flex items-center mb-10">
+                <div className="phone" />
+                <div className="flex flex-col space-y-2 text-secondary">
+                  <Link className="" href="">
+                    Amsterdam: 020-2440930
+                  </Link>
+                  <Link className="" href="">
+                    Rotterdam: 010-3075826
+                  </Link>
+                  <Link className="" href="">
+                    Den Haag: 070-2210162
+                  </Link>
+                </div>
+              </div>
+              <div className="w-full flex items-center">
+                <div className="location" />
+                <div className="flex flex-col space-y-2">
+                  <span>Hobbemakade 73, Amsterdam</span>
+                  <span>Hofplein 20, Rotterdam</span>
+                  <span>Prinsegracht 38, Den Haag</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1  pt-10">
+            <h3 className="uppercase  text-h3 text-darkGray mb-10">Steden</h3>
+            <div className="flex flex-col pt-3">
+              <div className="w-full flex mb-6 flex-col  space-y-3">
+                <Link href="/" className="">
+                  Gouden tip
+                </Link>
+                <Link href="/" className="">
+                  Kosten huis verkopen
+                </Link>
+                <Link href="/" className="">
+                  Verkopen zonder makelaar
+                </Link>
+                <Link href="/" className="">
+                  Huis zelf verkopen
+                </Link>
+              </div>
+              <h3 className="uppercase  text-h3 text-darkGray my-10">
+                Extra Informatie
+              </h3>
+              <div className="w-full flex mb-6 flex-col space-y-3">
+                <Link href="/" className="">
+                  Gouden tip
+                </Link>
+                <Link href="/" className="">
+                  Kosten huis verkopen
+                </Link>
+                <Link href="/" className="">
+                  Verkopen zonder makelaar
+                </Link>
+                <Link href="/" className="">
+                  Huis zelf verkopen
+                </Link>
+                <Link href="/" className="">
+                  Opkoper vastgoed
+                </Link>
+                <Link href="/" className="">
+                  Verhuurd verkopen
+                </Link>
+                <Link href="/" className="">
+                  Opkoper huis
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 bg-primary text-white text-center py-10">
+            <Link title="proranje" href="/">
+              <Image
+                decoding="async"
+                src="/images/proranje-footer-logo.png"
+                alt="randstad real estate logo"
+                width={200}
+                height={130}
+                className="mx-auto my-44"
+              />
+            </Link>
+            <div className="text-base flex-col flex space-y-2 font-poppins pb-20">
+              <Link
+                className="sitemap"
+                title="sitemap"
+                href="https://proranje.nl/sitemap/"
+              >
+                Sitemap
+              </Link>
+              <Link className="privacy " href="/privacy/">
+                Privacy
+              </Link>
+              <Link className="nieuws" href="/nieuws/">
+                Nieuws artikelen
+              </Link>
+              <Link className="vacatures" href="/vacatures/">
+                Vacatures
+              </Link>
+              <Link
+                className="veelgestelde-vragen"
+                href="/veelgestelde-vragen/"
+              >
+                Veelgestelde vragen
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,14 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        h3: "20px", // Custom font size for h3
+      },
       boxShadow: {
         custom: "0 1px 2px rgba(0, 0, 0, 0.1)",
       },
       fontFamily: {
         heading: ["Nunito", "sans-serif"], // Assuming you'll use Nunito for headings
         body: ["Poppins", "sans-serif"], // Assuming Poppins for body text
+        nunito: ["Nunito", "sans-serif"], // Assuming you'll use Nunito for headings
+        poppins: ["Poppins", "sans-serif"], // Assuming Poppins for body text
       },
       colors: {
+        primary: "#2e3e5e",
+        secondary: "#ea4517",
+        darkGray: "#3a3b3d",
         defaultText: "#6e6e6e", // Custom color named defaultText
         customGray: "#f7f7f7",
       },
